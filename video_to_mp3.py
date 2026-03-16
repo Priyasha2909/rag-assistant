7ba2c6d0bbd966a85f71ff0eeb2b61810c357dcf
Priyasha2909 <priyasha2909@gmail.com>
2026-03-16 19:37:21 +0530

    Added comments for the code

--- video_to_mp3.py
+++ video_to_mp3.py
@@ -1,9 +1,10 @@
-# Converts the videos to mp3 
+# Converts the videos to mp3 (audios) using FFmpeg so that Whisper can convert these audios into text
 import os 
 import subprocess
 
 files = os.listdir("videos") 
 for file in files: 
+    # To extract tutorial no. and fileName from videos
     tutorial_number = file.split(" [")[0].split(" #")[1]
     file_name = file.split(" ｜ ")[0]
     print( tutorial_number,  file_name)
