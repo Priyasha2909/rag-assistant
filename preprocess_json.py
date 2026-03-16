01f806d58e1f90a8d8e2864842079d71fe424f1d
Priyasha2909 <priyasha2909@gmail.com>
2026-03-16 20:04:36 +0530

    Updated file

--- preprocess_json.py
+++ preprocess_json.py
@@ -6,21 +6,22 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 import joblib
 
-def create_embedding(text_list):
-    # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
-    r = requests.post("http://localhost:11434/api/embed", json={
-        "model": "bge-m3",
-        "input": text_list
-    })
 
-    embedding = r.json()["embeddings"] 
-    return embedding
+# Using chunks to convert text into embeddings
+def create_embedding(text_list):
+        r = requests.post("http://localhost:11434/api/embed", json={
+            "model": "bge-m3",
+            "input": text_list
+        })
+
+        embedding = r.json()["embeddings"] 
+        return embedding
 
 
 jsons = os.listdir("jsons")  # List all the jsons 
 my_dicts = []
 chunk_id = 0
-
+# reading chunks 
 for json_file in jsons:
     with open(f"jsons/{json_file}") as f:
         content = json.load(f)
